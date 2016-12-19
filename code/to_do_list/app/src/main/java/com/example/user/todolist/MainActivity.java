e4cc8da6e9cb924a263a6d67372db279b87d6f5f
Wordybird <ben.a.faulkner@btinternet.com>
2016-12-19 15:14:25 +0000

    Replaced background image with a more fitting one, removed grey background on list items.

--- code/to_do_list/app/src/main/java/com/example/user/todolist/MainActivity.java
+++ code/to_do_list/app/src/main/java/com/example/user/todolist/MainActivity.java
@@ -2,10 +2,8 @@ package com.example.user.todolist;
 
 import java.util.ArrayList;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
